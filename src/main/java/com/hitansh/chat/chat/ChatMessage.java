69c753cb556faf6760c4515fc48bd393c77c7d84
Hitansh <hitansh.gupta@kristalball.com>
2023-12-28 21:48:41 +0530

    first commit

---
+++ src/main/java/com/hitansh/chat/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.hitansh.chat.chat;
+
+import lombok.*;
+import org.apache.logging.log4j.message.Message;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+}
---
+++ src/main/java/com/hitansh/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.hitansh.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
